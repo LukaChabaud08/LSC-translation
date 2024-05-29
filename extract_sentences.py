1557afc4dd71c55aef72cbb20d99203e1908150b
LukaChabaud <luka.chabaud01@estudiant.upf.edu>
2024-05-29 12:18:43 +0200

    Removed all non-manual components

--- extract_sentences.py
+++ extract_sentences.py
@@ -2,7 +2,7 @@ import os
 from argparse import ArgumentParser
 
 
-def main(input_dir: str, field: int) -> None:
+def main(input_dir: str, field: int, ignore_topic_marks: bool) -> None:
 
     if not os.path.isdir(input_dir):
         print(f"Path {input_dir} is not a valid directory")
@@ -23,6 +23,8 @@ def main(input_dir: str, field: int) -> None:
                 for line in lines:
                     tokens = line.split()
                     words = [token.split("|")[field] for token in tokens]
+                    if ignore_topic_marks:
+                        words = [word for word in words if word not in ["?!", "!", "?"]]
                     output_fp.write(" ".join(words) + "\n")
 
 
@@ -34,5 +36,6 @@ if __name__ == "__main__":
         help="path to the directory where the corpus files are located",
     )
     parser.add_argument("field", type=int)
+    parser.add_argument("--ignore_topic_marks", action="store_true")
     args = parser.parse_args()
-    main(args.input_directory, args.field)
+    main(args.input_directory, args.field, args.ignore_topic_marks)
